cc83dc2d7aa6fa794217ad67f2770830cedc6182
nadia1624 <nadyadearihanifah@gmail.com>
2025-01-14 09:35:46 +0700

    konfigurasi tailwind

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require("express");
-// const route = require('./routes/index')
+const route = require('./routes/index')
+const path = require('path');
 const bodyParser = require('body-parser')
 require('dotenv/config')
+
+
+
+
+
 
 
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json())
-// app.use(route)
+app.use(route)
+
+
+  
+// Middleware untuk melayani file statis dari folder "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./views/*.{html,js,ejs}',
+    'node_modules/preline/dist/*.js', // Path ke Preline plugin
+  ],
+  theme: {
+    extend : {
+      
+    }
+  },
+  plugins: [
+    require('preline/plugin'),
+  ],
+}
+

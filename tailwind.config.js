1c06248d909fdae21040fdcbcaded56767ad55ee
Rania Shofi <raniashofim13@gmail.com>
2025-01-14 12:29:33 +0700

    perbaikan login

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,14 @@ module.exports = {
   ],
   theme: {
     extend : {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        raleway: ['Raleway', 'sans-serif'],
+      },
       colors: {
         customBackground: '#F7F7F5', 
       },
-    }
+    },
   },
   plugins: [
     require('preline/plugin'),

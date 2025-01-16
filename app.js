a6ebe45b09f46b1219970e80442b2d22481e78c3
Rizka Kurnia Illahi <rizkakurniaillahi@gmail.com>
2025-01-16 10:33:25 +0700

    responsif

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// db.sync({alter: true})
+db.sync({alter: true})
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);

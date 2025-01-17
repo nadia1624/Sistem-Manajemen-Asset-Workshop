b70543827585ed684b78fbde6d18b16eec6ef094
nadia1624 <nadyadearihanifah@gmail.com>
2025-01-17 11:44:27 +0700

    pemeliharaan fe

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,16 @@ const pengembalianRoute = require('./pengembalianRoute.js')
 const permintaanRoute = require('./permintaanRoute.js')
 const pengajuanCekRouter = require('./pengajuanCek.js')
 const pengelolaanAsetRoute = require('./pengelolaanAset');
+const pemeliharaanRouter = require('./pemeliharaanRoute.js')
 
 const router = express.Router()
 
 router.use(authRoute)
 router.use(pengembalianRoute)
-
 router.use(permintaanRoute)
 router.use(pengajuanCekRouter)
-router.use(pengelolaanAsetRoute);
+router.use(pengelolaanAsetRoute)
+router.use(pemeliharaanRouter)
 
 
 module.exports = router
